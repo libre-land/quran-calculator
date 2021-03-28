f33f8e2bf86cbe838c5a3da76d417f5ba993696f
libre-land <steadstamina@gmail.com>
2021-03-28 04:06:05 -0700

    added ViewBinding

--- app/src/main/java/com/example/quran_calculator/MainActivity.kt
+++ app/src/main/java/com/example/quran_calculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.quran_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quran_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
     }
 }
