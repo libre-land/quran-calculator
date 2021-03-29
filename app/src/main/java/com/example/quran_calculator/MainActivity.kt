5bee9574b3fe49093a377f57ad0813d9856de47c
libre-land <steadstamina@gmail.com>
2021-03-28 22:44:45 -0700

    added basic logic

--- app/src/main/java/com/example/quran_calculator/MainActivity.kt
+++ app/src/main/java/com/example/quran_calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.quran_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.quran_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+
+        bindingClass.resultButton.setOnClickListener {
+            val pageInput = bindingClass.tvPageInput.text.toString().toInt()
+            val prayerInput = bindingClass.tvPrayerInput.text.toString().toInt()
+            val result = pageInput/prayerInput
+
+            bindingClass.tvResult.text = "Вы должны читать ${result} страниц в каждой молитве"
+            bindingClass.tvResult.visibility = View.VISIBLE
+        }
+
     }
 }
