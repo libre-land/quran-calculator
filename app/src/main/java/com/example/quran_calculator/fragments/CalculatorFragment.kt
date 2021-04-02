75e445024f585a7b007da1539824567a6c190734
libre-land <steadstamina@gmail.com>
2021-04-02 11:47:03 -0700

    changed clickListener location

--- app/src/main/java/com/example/quran_calculator/fragments/CalculatorFragment.kt
+++ app/src/main/java/com/example/quran_calculator/fragments/CalculatorFragment.kt
@@ -5,15 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.quran_calculator.R
 
 class CalculatorFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_calculator, container, false)
+        var resultButton = v.findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+            val pageInput = v.findViewById<EditText>(R.id.tvPageInput).text.toString().toInt()
+            val prayerInput = v.findViewById<EditText>(R.id.tvPrayerInput).text.toString().toInt()
+            val dayInput = v.findViewById<EditText>(R.id.tvDayInput).text.toString().toInt()
+            val tvResult = v.findViewById<TextView>(R.id.tvResult)
+
+            var result = 0
+
+            fun mathResult(result: Int): Any {
+                tvResult.visibility = View.VISIBLE
+
+                var result = try {
+                    (pageInput / prayerInput) / dayInput
+                } catch (ex: Exception) {
+                }
+
+                return result
+            }
+
+            when (mathResult(result)) {
+                1 -> {
+                    tvResult.text = "Вы должны читать ${mathResult(result)} страницу в каждой молитве"
+                }
+
+                in 2..4 -> {
+                    tvResult.text = "Вы должны читать ${mathResult(result)} страницы в каждой молитве"
+                }
+
+                in 5..100000 -> {
+                    tvResult.text = "Вы должны читать ${mathResult(result)} страниц в каждой молитве"
+                }
+
+                else -> {
+                    tvResult.text = "Не удалось вычислить"
+                    tvResult.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        return v
     }
 }
