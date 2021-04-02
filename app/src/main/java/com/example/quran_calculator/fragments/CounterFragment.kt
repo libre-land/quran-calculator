2f108356a5b21a7a2cb63dbc71cea77149d43a3d
libre-land <steadstamina@gmail.com>
2021-04-02 14:12:38 -0700

    added simple counter app

--- app/src/main/java/com/example/quran_calculator/fragments/CounterFragment.kt
+++ app/src/main/java/com/example/quran_calculator/fragments/CounterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.example.quran_calculator.R
 
 class CounterFragment : Fragment() {
@@ -14,6 +16,17 @@ class CounterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_counter, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_counter, container, false)
+
+        var tvCounter = v.findViewById<TextView>(R.id.tvCounter)
+        val counterPage = v.findViewById<RelativeLayout>(R.id.counterPage)
+        var count = 0
+
+        counterPage.setOnClickListener {
+            count++
+            tvCounter.text = count.toString()
+        }
+
+        return v
     }
 }
